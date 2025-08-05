7613258c1456547b3605a671e974a37123d02064
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 10:23:52 +0200

    implemented conditional sentence.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ import MovieCard from './components/MovieCard'
 
 
 function App() {
+ const movieNumber = 1;
+
  
 
   return (
     <>
-          <MovieCard movie={{title:"Tim's film",release_date:"2024"}}/>
+        {movieNumber === 1 ? (
+ <MovieCard movie={{title:"Tim's film",release_date:"2024"}}/>
+        ): (
+<MovieCard movie={{title:"Joe's film",release_date:"2022"}}/>
+        )} 
+          
     </>
-  )
+  );
 }
 
 export default App
