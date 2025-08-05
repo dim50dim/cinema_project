28807abc87e470394496a9fe08b989ea2cb30ef3
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 20:29:00 +0200

    created function getPopularMovies , which is async function and contain all logic.

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,8 @@
 const API_KEY = 'acd44edbeee7f1770df45d8dedd4ee27';
-const BASE_URL = 'https://api.themovieedb.org/3'
+const BASE_URL = 'https://api.themovieedb.org/3';
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+}
