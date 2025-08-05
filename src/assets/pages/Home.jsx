b5f28626d8971379179b24fbe0771f41a5a5c8b8
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 10:32:28 +0200

    added array to Home component , it is sort of hardware thing.

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+    const movies = [
+        {id:1, title: 'John Wick',release_date: '2020'},
+        {id:2, title: 'Terminator',release_date: '2025'},
+        {id:3, title: 'The Matrix',release_date: '2024'},
+    ]
   return (
-    <div>
+    <div className='home'>
       
     </div>
   )
