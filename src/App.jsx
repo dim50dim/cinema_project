ef49183d93901164ba39955ecd64d1d9a93e4b71
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 09:59:41 +0200

    created folder components , which is pretty usual for React project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import Text from './components/Text'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
              <h1>hello everyone, stay calm</h1>
+             <Text/>
     </>
   )
 }
