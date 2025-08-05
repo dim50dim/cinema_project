5a0063f2eff12aac674727ad367dfd1a42872792
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 16:20:21 +0200

    fulfill Favorites component.

--- src/assets/pages/Favorites.jsx
+++ src/assets/pages/Favorites.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Favorites = () => {
   return (
     <div>
-      
+      <h2>No favorites movies yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
     </div>
   )
 }
