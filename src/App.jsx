35bd16be2cc5921abc3dffd5c38a15cc88f36bc7
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-05 16:28:34 +0200

    made two Routes to components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 
 import './App.css'
+import Favorites from './assets/pages/Favorites';
 import Home from './assets/pages/Home';
 import MovieCard from './components/MovieCard'
-
+import{Routes,Route} from "react-router-dom"
 
 function App() {
 
@@ -12,7 +13,12 @@ function App() {
 
   return (
     <>
-           <Home/>
+  <main className="main-content">
+    <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/favorites' element={<Favorites/>}></Route>
+    </Routes>
+  </main>
     </>
   );
 }
