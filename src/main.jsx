7a29799d4e6d51b3c0a803356abf964a3083c4e2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-07 18:03:24 +0200

    added icon video-player.png to head in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import App from './App.jsx'
 import { HashRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-
+<StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-
+</StrictMode>
 )
