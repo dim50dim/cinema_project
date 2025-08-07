805dee4608384702f5557e2115894f970ec3ef26
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-07 15:15:58 +0200

    changed all imports in main.jsx - because Git does not recognize BrowserRouter , other words I need to use HasRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot} from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
-import{BrowserRouter} from "react-router-dom"
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
 // import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import '../src/css/index.css'
+// import { createRoot} from 'react-dom/client'
+// import './css/index.css'
 // import App from './App.jsx'
-// import { HashRouter } from "react-router-dom"
-
+// import{BrowserRouter} from "react-router-dom"
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
+//     <BrowserRouter>
+//      <App />
+//     </BrowserRouter>
 //   </StrictMode>,
 // )
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './css/index.css'
+import App from './App.jsx'
+import { HashRouter } from "react-router-dom"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
+)
