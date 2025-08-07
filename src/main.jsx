48c5f5dceb44babf1a2440363007a08f40c30d20
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-07 15:36:43 +0200

    deleted unnecessary lines of code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot} from 'react-dom/client'
-// import './css/index.css'
-// import App from './App.jsx'
-// import{BrowserRouter} from "react-router-dom"
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//      <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
