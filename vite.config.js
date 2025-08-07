dd891bfa3b3c256dbf5c2c5cc645845cc4e371f1
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-07 15:09:30 +0200

    rewrite one more time vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/cinema_project/',  // ✅ this is the fix
   plugins: [react()],
-  base: '/cinema_project/',  // ← le nom exact de ton repo GitHub
 })
